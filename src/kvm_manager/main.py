a26bfcbaba89be3dbbff0f719046a086b76637ef
Jeroen Doggen <jeroendoggen@gmail.com>
2014-10-13 17:52:28 +0200

    Close when ctrl-C pressed

--- src/kvm_manager/main.py
+++ src/kvm_manager/main.py
@@ -4,16 +4,20 @@
 """
 
 import sys
+import signal
 
 from manager import KVMManager
 
 
 def run():
     """Run the main program"""
+    signal.signal(signal.SIGINT, signal_handler)
     manager = KVMManager()
     manager.run()
     return(manager.exit_value())
 
+def signal_handler(signal, frame):
+        sys.exit(0)
 
 if __name__ == "__main__":
     sys.exit(run())
